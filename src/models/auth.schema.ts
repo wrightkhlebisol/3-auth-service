ed95b2d606dc28a3ab906183da89b554b879616a
wrightkhlebisol <calebogundiya@gmail.com>
2024-05-19 09:30:21 +0100

    feat: auth model

---
+++ src/models/auth.schema.ts
@@ -0,0 +1,56 @@
+import { sequelize } from '@auth/database';
+import { IAuthDocument } from '@wrightkhlebisol/jobber-shared';
+import { DataTypes, ModelDefined, Optional } from 'sequelize';
+
+type AuthUserCreationAttributes = Optional<IAuthDocument, 'id' | 'createdAt' | 'passwordResetToken' | 'passwordResetExpires'>;
+
+const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> = sequelize.define('auths', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profilePublicId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profilePicture: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  emailVerificationToken: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: Date.now(),
+  },
+  passwordResetToken: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  passwordResetExpires: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: Date.now,
+  }
+});
+
+export default AuthModel;
