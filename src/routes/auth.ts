d97da07a7b8bc713e9e1a8e558cfdca981dd79d9
wrightkhlebisol <calebogundiya@gmail.com>
2024-05-27 10:41:22 +0100

    feat: setup signup route

---
+++ src/routes/auth.ts
@@ -0,0 +1,10 @@
+import { create } from '@auth/controllers/signup';
+import express, { Router } from 'express';
+
+const router: Router = express.Router();
+
+export function authRoutes(): Router {
+  router.post('/signup', create);
+
+  return router;
+}
