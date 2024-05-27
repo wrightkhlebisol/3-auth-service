8798f9fa748445c399d9f700fccbd41a2fa0df43
wrightkhlebisol <calebogundiya@gmail.com>
2024-05-27 10:41:51 +0100

    feat: mount auth routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Application } from 'express';
+import { verifyGatewayRequest } from '@wrightkhlebisol/jobber-shared';
+
+import { authRoutes } from './routes/auth';
+
+
+const BASE_PATH = '/api/v1/auth';
 
 export function appRoutes(app: Application): void {
-  app.use('', () => console.log('appRoutes'));
+  app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
 }
