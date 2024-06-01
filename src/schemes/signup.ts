2338c1322d48cd39de2d13559944153b93888942
wrightkhlebisol <calebogundiya@gmail.com>
2024-06-01 13:12:12 +0100

    feat(signup validation): add descriptive messages

--- src/schemes/signup.ts
+++ src/schemes/signup.ts
@@ -3,14 +3,14 @@ import Joi, { ObjectSchema } from 'joi';
 const signupSchema: ObjectSchema = Joi.object().keys({
   username: Joi.string().min(4).max(12).required().messages({
     'string.base': 'Username must be of type string',
-    'string.min': 'Invalid username',
-    'string.max': 'Invalid username',
+    'string.min': 'Username length should be greater than or equal to 4',
+    'string.max': 'Username length should be lesser than or equal to 12',
     'string.empty': 'Username is a required field'
   }),
   password: Joi.string().min(4).max(12).required().messages({
     'string.base': 'Password must be of type string',
-    'string.min': 'Invalid password',
-    'string.max': 'Invalid password',
+    'string.min': 'Password length should be greater or equal to 4',
+    'string.max': 'Password length should be lesser or equal to 12',
     'string.empty': 'Password is a required field'
   }),
   country: Joi.string().required().messages({
@@ -22,11 +22,13 @@ const signupSchema: ObjectSchema = Joi.object().keys({
     'string.email': 'Invalid email',
     'string.empty': 'Email is a required field'
   }),
-  profilePicture: Joi.string().base64().required().messages({
-    'string.base': 'Please add a profile picture',
-    'string.base64': 'Profile picture is required',
-    'string.empty': 'Profile picture is required'
-  }),
+  profilePicture: Joi.string()
+    // .base64()
+    .required().messages({
+      'string.base': 'Please add a profile picture',
+      'string.base64': 'Profile picture is not a valid base64 string',
+      'string.empty': 'Profile picture is required'
+    }),
 });
 
 export { signupSchema };
