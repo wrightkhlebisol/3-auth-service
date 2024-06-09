225786dc00de00f90b64261eff3784b274c3212f
wrightkhlebisol <calebogundiya@gmail.com>
2024-06-09 10:47:04 +0100

    feat: mount signin, verify, forgot, reset and change password routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,18 @@
+import { read } from '@auth/controllers/signin';
 import { create } from '@auth/controllers/signup';
+import { verifyEmail } from '@auth/controllers/verify-email';
+import { forgotPassword, resetPassword, changePassword } from '@auth/controllers/password';
 import express, { Router } from 'express';
 
 const router: Router = express.Router();
 
 export function authRoutes(): Router {
   router.post('/signup', create);
+  router.post('/signin', read);
+  router.put('/verify-email', verifyEmail);
+  router.put('/forgot-password', forgotPassword);
+  router.put('/reset-password/:token', resetPassword);
+  router.put('/change-password', changePassword);
 
   return router;
 }
